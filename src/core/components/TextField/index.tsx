8bebfaeb2eaf18e7e8576778bd54c9d629114be0
JoseMartInez2611 <JoseMartinez2611@proton.me>
2025-03-24 18:36:08 -0500

    Feat: css and validations from loginpage functional, also adding the modify  component TextField to be used on the login, register and more adding the ts file about the interface from LoginLayout and TextField . Fix: Fixed some regular css from loginlayout

---
+++ src/core/components/TextField/index.tsx
@@ -0,0 +1,72 @@
+import { TextFileProps } from './TextField.interface';
+import { useState } from "react";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import { FC } from "react";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+export const Input: FC<TextFileProps> = function({ 
+  id = "outlined-basic", 
+  label, 
+  variant = "outlined", 
+  type = "text",
+  icon,
+  value, // Agregado para manejar el valor del input
+  onChange, // Agregado para manejar el cambio del input
+  required = false // Agregado para manejar si el campo es requerido
+}) {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(prev => !prev);
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const inputType = type === "password" && showPassword ? "text" : type;
+
+  // Preparar el adorno de finalización para contraseñas
+  const endAdornment = type === "password" ? (
+    <InputAdornment position="end">
+      <IconButton
+        aria-label="toggle password visibility"
+        onClick={handleClickShowPassword}
+        onMouseDown={handleMouseDownPassword}
+        edge="end"
+      >
+        {showPassword ? (
+          <VisibilityOffIcon fontSize="large" />
+        ) : (
+          <VisibilityIcon fontSize="large" />
+        )}
+      </IconButton>
+    </InputAdornment>
+  ) : null;
+
+  return (
+    <TextField 
+      className="TextField"
+      id={id} 
+      label={label} 
+      type={inputType}
+      variant={variant} 
+      value={value} // Asignar el valor del input
+      onChange={onChange} // Asignar el manejador de cambios
+      required={required} // Asignar si el campo es requerido
+      sx={{ bgcolor: "#ececec", width: "90%" }}
+      slotProps={{
+        input: {
+          startAdornment: icon ? (
+            <InputAdornment position="start">
+              <img src={icon} alt="icon" style={{ width: 20, height: 20 }} />
+            </InputAdornment>
+          ) : null,
+          endAdornment: endAdornment
+        }
+      }}
+      margin="dense"
+    />
+  );
+}
