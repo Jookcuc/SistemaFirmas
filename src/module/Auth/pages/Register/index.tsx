fcb98bec586e5688f58b86c83d2208dcdc149739
JulianGuardian <julguardian@proton.me>
2025-03-25 11:54:24 -0500

    fix: Replaces the base TextFields with the customs

--- src/module/Auth/pages/Register/index.tsx
+++ src/module/Auth/pages/Register/index.tsx
@@ -1,9 +1,13 @@
-import { Avatar, Box, Button, Divider, InputAdornment, Step, StepButton, Stepper, TextField } from '@mui/material'
+import { Box, Button, Divider, InputAdornment, Step, StepButton, Stepper } from '@mui/material'
 import { LoginLayout } from '../../../../core'
 import './Register.css'
 import { useState } from 'react'
-import { SignatureModal } from '../../../../core/components';
-import EditIcon from '@mui/icons-material/Edit';
+import { Input, SignatureModal } from '../../../../core/components';
+import EmailIcon from '../../../../core/icon/IconsRegister/EmailIcon.svg';
+import KeyIcon from '../../../../core/icon/IconsRegister/KeyIcon.svg';
+import PenIcon from '../../../../core/icon/IconsRegister/PenIcon.svg';
+import UserIcon from '../../../../core/icon/IconsRegister/UserIcon.svg';
+
 
 const steps = [
   "Datos Personales",
@@ -13,27 +17,37 @@ const steps = [
 
 export const RegisterPage = () => {
 
+  const [name, setName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [key, setKey] = useState("")
   const [firm, setFirm] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
   const [open, setOpen] = useState(false);
   const [activeStep, setActiveStep] = useState(0);
-  const [completed, setCompleted] = useState<{
-    [k: number]: boolean
-  }>({});
+  const [completed, setCompleted] = useState< {[k: number]: boolean} >({});
 
-  const handleOpen = () => () => {
+  const handleOpen = () => {
     setOpen(true);
   }
 
   const handleClose = () => () => setOpen(false);
 
-  const handleNext = () => () => {
-    setCompleted({
-      [0]: true,
-      [1]: false
-    })
-    setActiveStep(1)
+  const handleNext = () => {
+    if(name !== "" && lastName !== "" && key !== "" && firm !== ""){
+      setCompleted({
+        [0]: true,
+        [1]: false
+      })
+      setActiveStep(1)
+    }
+    else{
+
+    }
   }
-  const handleStep = (step: number) => () => {
+  const handleStep = (step: number) => {
     handleComplete(step)
     setActiveStep(step)
   }
@@ -46,34 +60,78 @@ export const RegisterPage = () => {
     });
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); 
+    console.log('Name:', name);
+    console.log('Last Name:', lastName);
+    console.log('Product Key:', key);
+    console.log('Firm:', firm);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+  };
+
   return (
     <LoginLayout title="Creación de Cuenta">
-      <Box component="form" sx={{display: "flex", paddingTop:"0"}}>
+      <Box component="form" sx={{display: "flex", paddingTop:"0"}} onSubmit={handleSubmit}>
         <Box className="formStep" id="registerStep2" sx={{display: activeStep === 0 ? "flex": "none"}}>
-          <TextField id='puto'></TextField>
-          <TextField></TextField>
-          <TextField></TextField>
+          
+          <Input
+            id="name"
+            label="Name"
+            type="text"
+            icon={UserIcon}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          
+          <Input
+            id="lastName"
+            label="Last Name"
+            type="text"
+            icon={UserIcon}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+
+          <Input
+            id="key"
+            label="Product Key"
+            type="text"
+            icon={KeyIcon}
+            value={key}
+            onChange={(e) => setKey(e.target.value)}
+            required
+          />
+
           <Box className="firmBox">
             <Box 
               sx={{
                 display:"flex",
+                justifyContent: "flex-start",
                 alignItems: "center",
                 gap: "1rem",
                 width: "70%", 
                 paddingX: "0.7rem",
-                border:"solid 2px #cdcdcd",
+                border:"solid 1px #b5b5b5",
                 borderRadius:"8px",
                 backgroundColor:"#ececec"
               }}
             >
-              <EditIcon />
+              <InputAdornment position="start">
+                <img src={PenIcon} alt="icon" style={{ minWidth: 20, minHeight: 20, maxWidth: 20, maxHeight: 20 }} />
+              </InputAdornment>
+
               <img src={firm} style={{maxHeight: "6vh"}}/>
             </Box>
-            <Button variant="contained" onClick={handleOpen()} sx={{width:"30%"}}>
+
+            <Button variant="contained" onClick={handleOpen} sx={{width:"30%"}}>
               Firmar
             </Button>
           </Box>
-          <Button variant="contained" onClick={handleNext()} className="stepperRegister"> Siguiente </Button>
+          <Button variant="contained" onClick={handleNext} className="stepperRegister"> Siguiente </Button>
         </Box>
 
         <Divider 
@@ -87,17 +145,44 @@ export const RegisterPage = () => {
           }}/>
 
         <Box className="formStep inactive" id="registerStep2" sx={{display: activeStep === 1 ? "flex": "none"}}>
-          <TextField></TextField>
-          <TextField></TextField>
-          <TextField></TextField>
-          <Button variant="contained"> Registrarse </Button>
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            icon={EmailIcon}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            icon={KeyIcon}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <Input
+            id="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            icon={KeyIcon}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+
+          <Button type="submit" variant="contained"> Registrarse </Button>
         </Box>
       </Box>
 
       <Stepper nonLinear activeStep={activeStep} alternativeLabel className="stepperRegister">
         {steps.map((label, index) => (
           <Step key={label} completed={completed[index]}>
-            <StepButton color="inherit" onClick={handleStep(index)}>
+            <StepButton color="inherit" onClick={() => handleStep(index)}>
               {label}
             </StepButton>
           </Step>
